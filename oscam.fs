51f37659573b53753109ea306c2a7fd15f2aa3b6
anow2008 <92413624+anow2008@users.noreply.github.com>
2026-04-03 00:33:58 +0200

    Add files via upload

---
+++ oscam.fs
@@ -0,0 +1,21 @@
+#################################################################################
+####                    oscam.fs Patched By: Youchie                         ####
+####                       https://t.me/Youchie                              ####
+#################################################################################
+#                             oscam.fs == CCcam.cfg                             #
+#                                                                               #
+# C: <hostname> <port> <username> <password> <wantemus>                         #
+# N: <ip> <port> <username> <pass> <des(14byte)> <nr_of_hops_away (default: 1)> #
+# R: <ip> <port> <ca4> <id6> <nr_of_hops_away (default: 1)>                     #
+# L: <ip> <port> <username> <pass> <ca4> <id6> <nr_of_hops_away (default: 1)>   #
+#                                                                               #
+# http://<url>/... # (C, N: load rows from the URL using libcurl ...)           #
+#                                                                               #
+# Line Enable/Disable =? (" # ")                                                #
+#################################################################################
+#
+#http://www.freecline.com/companies/Bis_TV
+# etc...
+####### preset servers #########
+##    SERVER0 to SERVER20     ##
+################################
